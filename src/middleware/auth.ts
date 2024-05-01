7d918da40caca92dc1403703a4112ceed1a29e78
orzarhi <or.zarhi11@gmail.com>
2024-05-01 11:12:51 +0300

    Refactor authMiddleware to use environment variables for verification

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,15 @@
 import { basicAuth } from 'hono/basic-auth'
+import { env } from 'hono/adapter'
+
+type Environment = {
+    USERNAME: string;
+    PASSWORD: string;
+}
 
 export const authMiddleware = basicAuth({
-    verifyUser: (username, password, c) =>
-        username === c.env.USERNAME && password === c.env.PASSWORD
+    verifyUser: (username, password, c) => {
+        const { USERNAME, PASSWORD } = env<Environment>(c)
+
+        return username === USERNAME && password === PASSWORD
+    }
 })
