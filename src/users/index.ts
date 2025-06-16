ded55e0d8a7b26d37d206f57ec38b7aa95a19ef0
orzarhi <or.zarhi11@gmail.com>
2025-06-16 15:33:59 +0300

    Add error logging for user subscription route and temporarily disable rate limiting checks

--- src/users/index.ts
+++ src/users/index.ts
@@ -11,6 +11,7 @@ appUser.get("/", authMiddleware, async (c) => {
     const { data, error } = await supabase.from('users').select('*')
 
     if (error) {
+        console.log("Error: ", error)
         return c.json(error.message, { status: 500 })
     }
 
@@ -19,34 +20,38 @@ appUser.get("/", authMiddleware, async (c) => {
 
 appUser.post('/subscribe', userValidator, async (c) => {
     try {
-        const ratelimit = c.get('ratelimit')
-
-        const ip = c.req.raw.headers.get('CF-Connecting-IP')
-
-        const { success } = await ratelimit.limit(ip ?? 'anonymous')
-
-        if (!success) {
-            return c.json('Rate limit exceeded', { status: 429 })
-        }
-
-        const body = await c.req.json()
-
-        const supabase = getSupabase(c)
-
-        const { data } = await supabase.from('users').select('*').eq('email', body.email)
-
-        if (data?.length) {
-            return c.json('Email already exists', { status: 403 })
-        }
-
-        const { error } = await supabase.from('users').insert(body)
-
-        if (error) {
-            return c.json(error.message, { status: 500 })
-        }
+        await new Promise(resolve => setTimeout(resolve, 2000))
 
         return c.json('Subscribed successfully', { status: 201 })
+        // const ratelimit = c.get('ratelimit')
+
+        // const ip = c.req.raw.headers.get('CF-Connecting-IP')
+
+        // const { success } = await ratelimit.limit(ip ?? 'anonymous')
+
+        // if (!success) {
+        //     return c.json('Rate limit exceeded', { status: 429 })
+        // }
+
+        // const body = await c.req.json()
+
+        // const supabase = getSupabase(c)
+
+        // const { data } = await supabase.from('users').select('*').eq('email', body.email)
+
+        // if (data?.length) {
+        //     return c.json('Email already exists', { status: 403 })
+        // }
+
+        // const { error } = await supabase.from('users').insert(body)
+
+        // if (error) {
+        //     return c.json(error.message, { status: 500 })
+        // }
+
+        // return c.json('Subscribed successfully', { status: 201 })
     } catch (error) {
+        console.log("Error: ", error)
         return c.json(error, { status: 500 })
     }
 })
