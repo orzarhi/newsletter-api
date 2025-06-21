aba3f0923fff141ed41c90e10693bd0b54c8ae9f
orzarhi <or.zarhi11@gmail.com>
2025-06-22 00:49:58 +0300

    Refactor user routes in index.ts for improved readability and consistency, including formatting adjustments and error handling enhancements.

--- src/users/index.ts
+++ src/users/index.ts
@@ -1,59 +1,61 @@
-import { Hono } from "hono";
-import { authMiddleware, getSupabase, ratelimitMiddleware, supabaseMiddleware } from "../middleware";
-import { userValidator } from "./validator";
+import { Hono } from 'hono';
+import {
+  authMiddleware,
+  getSupabase,
+  ratelimitMiddleware,
+  supabaseMiddleware,
+} from '../middleware';
+import { userValidator } from './validator';
 
 const appUser = new Hono();
 
-appUser.use('*', supabaseMiddleware, ratelimitMiddleware)
+appUser.use('*', supabaseMiddleware, ratelimitMiddleware);
 
-appUser.get("/", authMiddleware, async (c) => {
-    const supabase = getSupabase(c)
-    const { data, error } = await supabase.from('users').select('*')
+appUser.get('/', authMiddleware, async (c) => {
+  const supabase = getSupabase(c);
+  const { data, error } = await supabase.from('users').select('*');
 
-    if (error) {
-        console.log("Error: ", error)
-        return c.json(error.message, { status: 500 })
-    }
+  if (error) {
+    console.log('Error: ', error);
+    return c.json(error.message, { status: 500 });
+  }
 
-    return c.json(data, { status: 200 })
+  return c.json(data, { status: 200 });
 });
 
 appUser.post('/subscribe', userValidator, async (c) => {
-    try {
-        await new Promise(resolve => setTimeout(resolve, 2000))
+  try {
+    const ratelimit = c.get('ratelimit');
 
-        return c.json('Subscribed successfully', { status: 201 })
-        // const ratelimit = c.get('ratelimit')
+    const ip = c.req.raw.headers.get('CF-Connecting-IP');
 
-        // const ip = c.req.raw.headers.get('CF-Connecting-IP')
+    const { success } = await ratelimit.limit(ip ?? 'anonymous');
 
-        // const { success } = await ratelimit.limit(ip ?? 'anonymous')
-
-        // if (!success) {
-        //     return c.json('Rate limit exceeded', { status: 429 })
-        // }
-
-        // const body = await c.req.json()
-
-        // const supabase = getSupabase(c)
-
-        // const { data } = await supabase.from('users').select('*').eq('email', body.email)
-
-        // if (data?.length) {
-        //     return c.json('Email already exists', { status: 403 })
-        // }
-
-        // const { error } = await supabase.from('users').insert(body)
-
-        // if (error) {
-        //     return c.json(error.message, { status: 500 })
-        // }
-
-        // return c.json('Subscribed successfully', { status: 201 })
-    } catch (error) {
-        console.log("Error: ", error)
-        return c.json(error, { status: 500 })
+    if (!success) {
+      return c.json('Rate limit exceeded', { status: 429 });
     }
-})
+
+    const body = await c.req.json();
+
+    const supabase = getSupabase(c);
+
+    const { data } = await supabase.from('users').select('*').eq('email', body.email);
+
+    if (data?.length) {
+      return c.json('Email already exists', { status: 403 });
+    }
+
+    const { error } = await supabase.from('users').insert(body);
+
+    if (error) {
+      return c.json(error.message, { status: 500 });
+    }
+
+    return c.json('Subscribed successfully', { status: 201 });
+  } catch (error) {
+    console.log('Error: ', error);
+    return c.json(error, { status: 500 });
+  }
+});
 
 export default appUser;
